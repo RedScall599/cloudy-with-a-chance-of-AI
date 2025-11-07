23767a57afa345a2f0098e1045657b83bf342fe4
jose A <jalic0054@launchpadphilly.org>
2025-11-07 14:04:23 -0500

    vercel refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+// 2️⃣ Wrap your entire app in QueryClientProvider
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
